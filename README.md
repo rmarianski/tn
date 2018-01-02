c09584f132550dd8a6194aa78c926b333af1f2f2
Robert Marianski <rob@marianski.com>
2018-01-01 19:49:25 -0500

    Add option to remove any trailing newline

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ tn
 ==
 
 Copies stdin to stdout, but ensures that it ends with a trailing newline.
+
+With `-n` option, removes any trailing newline.
--- tn.c
+++ tn.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    int has_last_char = 0;
+    char last_char = 0;
     char buffer[4096];
-    size_t n_rd, n_wr = 0;
-    int rc = 0;
-    char *last_char;
+    size_t n_rd, n_wr;
+    int char_written;
+    char char_to_write;
+    int remove_newline = 0;
+
+    if (argc == 2) {
+        char *option = argv[1];
+        if (option[0] == '-' && option[1] == 'n') {
+            remove_newline = 1;
+        }
+    }
 
     for (;;) {
-        last_char = 0;
         n_rd = fread(buffer, 1, sizeof(buffer), stdin);
         if (n_rd > 0) {
-            last_char = buffer + n_rd - 1;
-            n_wr = fwrite(buffer, 1, n_rd, stdout);
-            if (n_wr != n_rd) {
-                perror("fwrite");
-                rc = 2;
+            if (remove_newline && has_last_char) {
+                char_written = fputc(last_char, stdout);
+                if (char_written == EOF) {
+                    perror("fputc");
+                    return 1;
+                }
+            }
+            has_last_char = 1;
+            last_char = buffer[n_rd - 1];
+            if (remove_newline) {
+                if (n_rd > 1) {
+                    n_wr = fwrite(buffer, 1, n_rd - 1, stdout);
+                    if (n_wr != n_rd - 1) {
+                        perror("fwrite");
+                        return 2;
+                    }
+                }
+            } else {
+                n_wr = fwrite(buffer, 1, n_rd, stdout);
+                if (n_wr != n_rd) {
+                    perror("fwrite");
+                    return 3;
+                }
             }
         }
         if (feof(stdin)) {
@@ -24,12 +52,15 @@ int main() {
             break;
         }
     }
-    if (last_char && *last_char != '\n') {
-        int written = fputc('\n', stdout);
-        if (written == EOF) {
+
+    if (has_last_char && last_char != '\n') {
+        char_to_write = remove_newline ? last_char : '\n';
+        char_written = fputc(char_to_write, stdout);
+        if (char_written == EOF) {
             perror("fputc");
-            rc = 3;
+            return 4;
         }
     }
-    return rc;
+
+    return 0;
 }
